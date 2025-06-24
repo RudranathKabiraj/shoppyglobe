13082025d77f0f8cf98b30d4e018f771c5bfcf0f
RudranathKabiraj <kabirajrudranath@gmail.com>
2025-06-24 23:50:36 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Update `base` with the name of your GitHub repository
 export default defineConfig({
+  base: '/shoppyglobe/',
   plugins: [react()],
 })
